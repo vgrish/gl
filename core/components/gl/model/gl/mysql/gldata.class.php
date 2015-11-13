e151d8fe9b8d31a806c2ec76cdd242b8af5fb775
Володя Володин <vovanblya@mail.ru>
2015-11-13 13:48:07 +0400

    - initial

---
+++ core/components/gl/model/gl/mysql/gldata.class.php
@@ -0,0 +1,3 @@
+<?php
+require_once (dirname(dirname(__FILE__)) . '/gldata.class.php');
+class glData_mysql extends glData {}
---
+++ core/components/gl/model/gl/mysql/gldata.map.inc.php
@@ -0,0 +1,112 @@
+<?php
+$xpdo_meta_map['glData']= array (
+  'package' => 'gl',
+  'version' => '1.1',
+  'table' => 'gl_data',
+  'extends' => 'xPDOSimpleObject',
+  'fields' => 
+  array (
+    'identifier' => 0,
+    'class' => 'glCity',
+    'phone' => NULL,
+    'email' => NULL,
+    'address' => NULL,
+    'default' => 0,
+    'properties' => NULL,
+  ),
+  'fieldMeta' => 
+  array (
+    'identifier' => 
+    array (
+      'dbtype' => 'int',
+      'precision' => '10',
+      'attributes' => 'unsigned',
+      'phptype' => 'integer',
+      'null' => false,
+      'default' => 0,
+    ),
+    'class' => 
+    array (
+      'dbtype' => 'varchar',
+      'precision' => '100',
+      'phptype' => 'string',
+      'null' => false,
+      'default' => 'glCity',
+    ),
+    'phone' => 
+    array (
+      'dbtype' => 'varchar',
+      'precision' => '20',
+      'phptype' => 'string',
+      'null' => true,
+    ),
+    'email' => 
+    array (
+      'dbtype' => 'varchar',
+      'precision' => '255',
+      'phptype' => 'string',
+      'null' => true,
+    ),
+    'address' => 
+    array (
+      'dbtype' => 'text',
+      'phptype' => 'string',
+      'null' => true,
+    ),
+    'default' => 
+    array (
+      'dbtype' => 'tinyint',
+      'precision' => '1',
+      'phptype' => 'boolean',
+      'null' => true,
+      'default' => 0,
+    ),
+    'properties' => 
+    array (
+      'dbtype' => 'text',
+      'phptype' => 'json',
+      'null' => true,
+    ),
+  ),
+  'indexes' => 
+  array (
+    'unique_key' => 
+    array (
+      'alias' => 'unique_key',
+      'primary' => false,
+      'unique' => true,
+      'type' => 'BTREE',
+      'columns' => 
+      array (
+        'identifier' => 
+        array (
+          'length' => '',
+          'collation' => 'A',
+          'null' => false,
+        ),
+        'class' => 
+        array (
+          'length' => '',
+          'collation' => 'A',
+          'null' => false,
+        ),
+      ),
+    ),
+    'default' => 
+    array (
+      'alias' => 'default',
+      'primary' => false,
+      'unique' => false,
+      'type' => 'BTREE',
+      'columns' => 
+      array (
+        'default' => 
+        array (
+          'length' => '',
+          'collation' => 'A',
+          'null' => false,
+        ),
+      ),
+    ),
+  ),
+);
